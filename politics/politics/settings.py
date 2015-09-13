4d89efd06dd54db40cfb1c32c60ef010356594a3
Lawrence Vanderpool <lawrence.vanderpool@gmail.com>
2015-09-12 19:45:29 -0500

    refactor settings.py to use dj_database_url

--- politics/politics/settings.py
+++ politics/politics/settings.py
@@ -94,7 +94,7 @@ STATICFILES_DIRS = (
         os.path.join(BASE_DIR, "src"),
     )
 
-DATABASES['default'] = dj_atabase_url.config()
+DATABASES['default'] = dj_database_url.config()
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
